4d0a34c6ec44f1106761ff56a66713515ac88d0f
William Moreno <williamjmorenor@gmail.com>
2020-07-20 17:24:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="open-marquesote",
-    version="0.0.1"
+    version="0.0.1",
     author="William José Moreno Reyes",
     author_email="williamjmorenor@gmail.com",
     description="Minimal Web Browser",
